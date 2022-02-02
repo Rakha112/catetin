49de9da947461238ceb9e9185373bb27cf5ac85e
Farid Fawwaz Rakha Wibowo <rakhawibowo1998@gmail.com>
2022-02-02 12:55:41 +0700

    change navbar

--- src/components/Navnote.js
+++ src/components/Navnote.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import logo from "../image/CATETIN2.png";
+// import logo from "../image/CATETIN2.png";
 import "../css/navnote.css";
 import logouser from "../image/user.png";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Savebtn from "./Btn";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 const Navnote = ({ user }) => {
   const [aktifOut, setAktifOut] = useState(false);
   const navigate = useNavigate();
@@ -39,16 +41,17 @@ const Navnote = ({ user }) => {
   };
   return (
     <div className="navnote">
-      <div className="logo" onClick={klik}>
-        <img src={logo} alt="" />
-        <h1>CATETIN</h1>
-      </div>
-      <div className="user" onClick={out}>
-        <h1>{user}</h1>
+      <div className="user">
         <img src={logouser} alt="user" />
+        <h1>{user}</h1>
         <div className={aktifOut ? "logout aktif" : "logout"}>
           <Savebtn nama={"Log out"} submit={submit} />
         </div>
+      </div>
+      <div className="logo" onClick={klik}>
+        <IconButton onClick={out}>
+          <MoreVertIcon />
+        </IconButton>
       </div>
     </div>
   );
